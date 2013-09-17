f6e723ad441e5fa8d57fc6a6b9c18dd800f38205
Javier Bértoli <javier@netmanagers.com.ar>
2013-09-17 16:20:23 -0300

    Refactoring

--- manifests/storage/device.pp
+++ manifests/storage/device.pp
@@ -1,30 +1,29 @@
 # This define APPENDS a line entry to a device file
 #
 define bacula::storage::device (
-  $device_name = '',
-  $mediaType = '',
+  $name = $title,
+  $media_type = '',
   $archive_device = '',
-  $label_media = '',
-  $ramdon_access = '',
-  $automatic_mount = '',
-  $removable_media = '' ,
-  $always_open = '',
-  $manage_file_content = ''{
+  $label_media = 'yes',
+  $random_access = 'yes',
+  $automatic_mount = 'yes',
+  $removable_media = 'no' ,
+  $always_open = 'false',
+  $template = 'templates/device.conf.erb'
+) { 
 
   include bacula
 
-file { 'device.conf':
+  file { "device-${name}.conf":
     ensure  => $bacula::manage_file,
-    path    => $bacula::config_file,
+    path    => "${storage_configs_dir}/device-${name}.conf",
     mode    => $bacula::config_file_mode,
     owner   => $bacula::config_file_owner,
     group   => $bacula::config_file_group,
-    require => Package['bacula'],
+    require => Package[$bacula::storage_package],
     notify  => $bacula::manage_service_autorestart,
-    source  => $bacula::manage_file_source,
-    content => $bacula::manage_file_content,
+    content => $template,
     replace => $bacula::manage_file_replace,
     audit   => $bacula::manage_audit,
   }
-
 }
