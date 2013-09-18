06ccd9e992048fce7660e507928367727b2d6e1e
Sebastián Quaino <sebastian@netmanagers.com.ar>
2013-09-18 18:06:37 -0300

    Still trying with the spec/defines/bacula_storage_device_spec.rb

--- spec/defines/bacula_storage_device_spec.rb
+++ spec/defines/bacula_storage_device_spec.rb
@@ -30,7 +30,8 @@ Removable Media = no
 Always Open = false
 "
     end
-    it { should contain_file('device.conf').with_content(expected) }
+#    it { should contain_file('device-sample1.conf').with_path('/etc/bacula/storage.d/device-sample1.conf').with_content(expected) }
+    it { should contain_file('device-sample1.conf').with_path('/device-sample1.conf').with_content(expected) }
   end
 
   describe 'Test device.conf is created with all main options' do
@@ -59,7 +60,7 @@ Always Open = false
 }
 "
     end
-    it { should contain_file('device.conf').with_content(expected) }
+    it { should contain_file('device-sample2.conf').with_path('/etc/bacula/storage.d/device-sample2.conf').with_content(expected) }
   end
 
 end
