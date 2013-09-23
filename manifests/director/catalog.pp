676ba46df550f2ec0e8c2ff61cd8150f85b85363
Sebastián Quaino <sebastian@netmanagers.com.ar>
2013-09-23 12:23:55 -0300

    Some refactor and add the spec for bacula_director_catalog_spec.rb

--- manifests/director/catalog.pp
+++ manifests/director/catalog.pp
@@ -6,20 +6,20 @@ define bacula::director::catalog (
   $db_name = '',
   $db_user = '',
   $db_password = '',
-  $template = "templates/catalog.conf.erb"
+  $template = "bacula/catalog.conf.erb"
 ) {
 
   include bacula
 
   file { "catalog-${name}.conf":
     ensure  => $bacula::manage_file,
-    path    => "${catalog_configs_dir}/catalog-${name}.conf",
+    path    => "${director_configs_dir}/catalog-${name}.conf",
     mode    => $bacula::config_file_mode,
     owner   => $bacula::config_file_owner,
     group   => $bacula::config_file_group,
     require => Package[$bacula::director_package],
     notify  => $bacula::manage_service_autorestart,
-    content => $template,
+    content => template($template),
     replace => $bacula::manage_file_replace,
     audit   => $bacula::manage_audit,
   }
