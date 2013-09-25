5a53d5d6d6df6a9b77917bf6cde0f43ea83aee6d
Sebastián Quaino <sebastian@netmanagers.com.ar>
2013-09-25 17:24:55 -0300

    Add one new spec the bacula_director_fileset_spec.rb file

---
+++ spec/defines/bacula_director_fileset_spec.rb
@@ -0,0 +1,70 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
+
+describe 'bacula::director::fileset' do
+
+  let(:title) { 'bacula::fileset' }
+  let(:node) { 'rspec.example42.com' }
+  let(:facts) do
+    {
+      :ipaddress       => '10.42.42.42',
+      :operatingsystem => 'Debian',
+      :director_configs_dir => '/etc/bacula/director.d',
+    }
+  end
+
+  describe 'Test fileset.conf is created with no options' do
+    let(:params) do
+      {
+        :name => 'sample1',
+      }
+    end
+    let(:expected) do
+'
+# This file is managed by Puppet. DO NOT EDIT.
+
+FileSet {
+Name = sample1
+  Include {
+    Options {
+      signature = 
+      compression = 
+      onefs = 
+    }
+  }
+  Exclude {
+  }
+}
+'
+    end
+    it { should contain_file('fileset-sample1.conf').with_path('/etc/bacula/director.d/fileset-sample1.conf').with_content(expected) }
+  end
+
+#  describe 'Test fileset.conf is created with all main options' do
+#    let(:params) do
+#      {
+#        :name => 'sample2',
+#      }
+#    end
+
+#    let(:expected) do
+#'# This file is managed by Puppet. DO NOT EDIT.
+
+#FileSet {
+#  Name = sample2
+#    Include {
+#      Options {
+#        signature =       
+#        compression =       
+#        onefs =
+#      }
+#    }
+#    Exclude {
+#   }
+#}
+#'
+#    end
+#    it { should contain_file('fileset-sample2.conf').with_path('/etc/bacula/director.d/fileset-sample2.conf').with_content(expected) }
+#  end
+
+end
+
