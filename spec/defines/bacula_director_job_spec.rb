556beaa7e944b521df6ad0ef42271602e3a616bf
Sebastián Quaino <sebastian@netmanagers.com.ar>
2013-09-26 17:15:51 -0300

    Add a new test for the job.pp file

---
+++ spec/defines/bacula_director_job_spec.rb
@@ -0,0 +1,75 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
+
+describe 'bacula::director::job' do
+
+  let(:title) { 'bacula::job' }
+  let(:node) { 'rspec.example42.com' }
+  let(:facts) do
+    {
+      :ipaddress       => '10.42.42.42',
+      :operatingsystem => 'Debian',
+      :director_configs_dir => '/etc/bacula/director.d',
+    }
+  end
+
+  describe 'Test job.conf is created with no options' do
+    let(:params) do
+      {
+        :name => 'sample1',
+      }
+    end
+    let(:expected) do
+'# This file is managed by Puppet. DO NOT EDIT.
+
+Job {
+  Name = sample1
+
+}
+'
+    end
+    it { should contain_file('job-sample1.conf').with_path('/etc/bacula/director.d/job-sample1.conf').with_content(expected) }
+  end
+
+#  describe 'Test job.conf is created with all main options' do
+#    let(:params) do
+#      {
+#        :name => 'sample2',
+#        :signature => 'MD5',
+#        :compression => 'GZIP',
+#        :onefs => 'no',
+#        :fstype => ['ext2','ext3','ext4'],
+#        :include => ['/home/sebastian','/etc'],
+#        :exclude => ['/proc','/sys','/temp'],
+#      }
+#    end
+
+#    let(:expected) do
+'# This file is managed by Puppet. DO NOT EDIT.
+
+#FileSet {
+#Name = sample2
+#  Include {
+#    Options {
+#      signature = MD5
+#      compression = GZIP
+#      onefs = no
+#      fstype = ext2
+#      fstype = ext3
+#      fstype = ext4
+#    }
+#    File = /home/sebastian
+#    File = /etc
+#  }
+#  Exclude {
+#    File = /proc
+#    File = /sys
+#    File = /tmp
+#  }
+#}
+#'
+#    end
+#    it { should contain_file('fileset-sample2.conf').with_path('/etc/bacula/director.d/fileset-sample2.conf').with_content(expected) }
+#  end
+
+end
+
