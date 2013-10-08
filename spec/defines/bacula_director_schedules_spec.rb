c061b3251bc22e4d2966c1dd381aa4973f7cf4cc
Sebastián Quaino <sebastian@netmanagers.com.ar>
2013-10-08 18:20:54 -0300

    Add the spec/defines/bacula_director_schedules_spec.rb

---
+++ spec/defines/bacula_director_schedules_spec.rb
@@ -0,0 +1,53 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
+
+describe 'bacula::director::schedule' do
+
+  let(:title) { 'bacula::schedule' }
+  let(:node) { 'rspec.example42.com' }
+  let(:facts) do
+    {
+      :ipaddress       => '10.42.42.42',
+      :operatingsystem => 'Debian',
+      :schedules_configs_dir => '/etc/bacula/director.d',
+    }
+  end
+
+  describe 'Test schedules.conf is created with no options' do
+    let(:params) do
+      {
+        :name => 'sample1',
+      }
+    end
+    let(:expected) do
+'# This file is managed by Puppet. DO NOT EDIT.
+
+Schedule {
+  Name = sample1
+}
+'
+    end
+    it { should contain_file('schedule-sample1.conf').with_path('/etc/bacula/director.d/schedule-sample1.conf').with_content(expected) }
+  end
+
+  describe 'Test schedules.conf is created with all main options' do
+    let(:params) do
+      {
+        :name => 'sample2',
+        :backup_level => [['Full','sun','22:00']],
+      }
+    end
+
+    let(:expected) do
+'# This file is managed by Puppet. DO NOT EDIT.
+
+Schedule {
+  Name = sample2
+  Run = Level=Full sun at 22:00
+}
+'
+    end
+    it { should contain_file('schedule-sample2.conf').with_path('/etc/bacula/director.d/schedule-sample2.conf').with_content(expected) }
+  end
+
+end
+
